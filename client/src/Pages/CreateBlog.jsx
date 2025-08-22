421cb186e5c6353704ad2f8471f96ec18a41f47c
kajal kanwar <kajalkanwar0208@gmail.com>
2025-08-22 12:21:13 -0400

    testing

--- client/src/Pages/CreateBlog.jsx
+++ client/src/Pages/CreateBlog.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-// import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
+import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 
 export const CreateBlog = () => {
