6a2a2aa9caad0edd5fc65918a89fa2a65e23d48f
kajal kanwar <kajalkanwar0208@gmail.com>
2025-08-22 13:12:01 -0400

    Edit Text

--- client/src/Pages/CreateBlog.jsx
+++ client/src/Pages/CreateBlog.jsx
@@ -139,7 +139,7 @@ export const CreateBlog = () => {
         border border-gray-200 dark:border-gray-800 p-8 transition-colors">
         {/* Title */}
         <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-gray-100 mb-8 tracking-wide">
-          Create New Post
+          New Post
         </h1>
 
         {/* Error */}
